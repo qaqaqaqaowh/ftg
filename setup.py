8cbc48a3f3581dbf48b93e5f9a84cb95b9274b97
Nicholas Ong <nicholasowh@hotmail.com>
2020-04-10 12:45:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     'Intended Audience :: Developers',   
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.7'
+    'Programming Language :: Python :: 3'
   ],
 )
